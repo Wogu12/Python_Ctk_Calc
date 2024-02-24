a0224e98665a040ef4d03f844f086434c7f242a1
Wogu12 <wogu129@gmail.com>
2024-02-24 14:14:44 +0100

    Added new functionality, menaged keyboard input, fixed bugs

--- main.py
+++ main.py
@@ -14,30 +14,37 @@ class Calc(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
-        self.title("my app")
-        self.grid_columnconfigure((0, 1), weight=0)
+        __window_width = self.winfo_width()
+        __window_height = self.winfo_height()
+
+        self.title("CTk Calculator")
 
         self.input_label = customtkinter.CTkLabel(self, text="", font=("Arial", 18))
         self.input_label.grid(row=0, column=0, columnspan=4, pady=10, sticky="e")
+        
+        self.bind('<Key>', self.key_pressed)
 
-        #Add C button
-        button = customtkinter.CTkButton(self, text="C", command=lambda btn_txt="C": self.button_callback(btn_txt), width=50, height=50)
-        button.grid(row=0, column=0, columnspan=1, pady=2, padx=2)
+        button = customtkinter.CTkButton(self, text="C", command=lambda btn_txt="C": self.button_callback(btn_txt), width=(__window_width/4), height=(__window_height/4))
+        button.grid(row=0, column=0, columnspan=1, pady=2, padx=2, sticky="nesw")
+
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
         
 
         for i, row in enumerate(self.__btn):
             for j, text in enumerate(row):
-                button = customtkinter.CTkButton(self, text=text, command=lambda btn_txt=text: self.button_callback(btn_txt), width=50, height=50)
-                button.grid(row=i+1, column=j, pady=2, padx=2)
+                button = customtkinter.CTkButton(self, text=text, command=lambda btn_txt=text: self.button_callback(btn_txt), width=(__window_width/4), height=(__window_height/4))
+                button.grid(row=i+1, column=j, pady=2, padx=2, sticky="nesw")
+                self.grid_rowconfigure(i+1, weight=1)
+                self.grid_columnconfigure(j, weight=1)
         
     def button_callback(self, btn_txt):
-        #window size
-        #print(self.winfo_width()) 
-        #print(self.winfo_height())
-        if btn_txt != "=" and btn_txt != "C":
+        if btn_txt != "=" and btn_txt != "\r" and btn_txt != "C" and btn_txt != "\b":
             self.append_button(btn_txt)
         elif btn_txt == "C":
-            self.clear_after_c(btn_txt)
+            self.clear_input() 
+        elif btn_txt == "\b":
+            self.remove_last_char()
         else:
             self.evaluate_expression()
 
@@ -45,27 +52,53 @@ class Calc(customtkinter.CTk):
         self.__pressed_buttons.append(btn_txt)
         self.update_input_label()
 
+    def input_is_clear(self):
+        return True if not self.__pressed_buttons else False
+
     def evaluate_expression(self):
-        try:
-            str_expr = "".join(self.__pressed_buttons)
-            result = eval(str_expr)
-            self.input_label.configure(text=f"{result}")
-        except Exception as e:
-            self.input_label.configure(text=f"Error: {e}")
+        if not self.input_is_clear():
+            try:
+                if self.check_equation():
+                    str_expr = "".join(self.__pressed_buttons)
+                    result = eval(str_expr)
+                    self.input_label.configure(text=f"{result}")
+                    self.__pressed_buttons = list(str(result))
+                    
+            except Exception as e:
+                self.input_label.configure(text=f"Error: {e}")
+        else:
+            self.clear_input()
+
+    def check_equation(self):
+        return False if self.__pressed_buttons[-1] in "+-*/." else True
 
     def update_input_label(self):
-        self.input_label.configure(text=" ".join(self.__pressed_buttons))
-
-    def clear_after_c(self, btn_txt):
-        self.__pressed_buttons.clear()
         self.input_label.configure(text="".join(self.__pressed_buttons))
 
+    def clear_input(self):
+        self.__pressed_buttons.clear()
+        self.update_input_label()
+
+    def key_pressed(self, event):
+        key = event.char
+        if key.isdigit() or key in "+-*/.\r\b":
+            self.button_callback(key)
+    
+    def remove_last_char(self):
+        self.__pressed_buttons.pop()
+        self.update_input_label()
 
 
-app = Calc()
-app.mainloop()
+if __name__=="__main__":
+    app = Calc()
+    app.mainloop()
 
 #################### TO DO ####################
 #
-# 1. make it responsive!
-# 2. add more options
+# 1. make it responsive! - DONE
+# 2. add keyboard input - DONE
+# 3. prevent wrong inputs - DONE
+# 4. add backspace option - DONE
+# 5. add more options
+#
+################################################
